d0e09d5c86e071b711188eaa838f463b2f341fb5
luht <luht@developbranch.cn>
2022-01-11 14:00:48 +0800

    feat: arrange labels on restaurant screen and link them to controller code, click map annotation to show the detail screen

--- Eat/RestaurantDetail/RestaurantDetailViewController.swift
+++ Eat/RestaurantDetail/RestaurantDetailViewController.swift
@@ -6,88 +6,107 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantDetailViewController: UITableViewController {
+    
+    @IBOutlet weak var btnHeart: UIBarButtonItem!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblCuisine: UILabel!
+    @IBOutlet weak var lblHeaderAddress: UILabel!
+    @IBOutlet weak var lblTableDetails: UILabel!
+    @IBOutlet weak var lblOverallRating: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
+    @IBOutlet weak var imgMap: UIImageView!
     
     var selectedRestaurent: RestaurantItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialize()
+        //dump(selectedRestaurent as Any)
+    }
+
+}
+
+private extension RestaurantDetailViewController {
+    func initialize() {
+        setupLabels()
+        createMap()
+    }
+    
+    func setupLabels() {
+        guard let restaurant = selectedRestaurent else {
+            return
+        }
         
-        dump(selectedRestaurent as Any)
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        if let name = restaurant.name {
+            lblName.text = name
+            title = name
+        }
+        
+        if let cuisine = restaurant.subtitle {
+            lblCuisine.text = cuisine
+        }
+        
+        if let address = restaurant.address {
+            lblAddress.text = address
+            lblHeaderAddress.text = address
+        }
+        
+        lblTableDetails.text = "Table for 7, tonight at 10:00 PM"
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func createMap() {
+        guard let annotation = selectedRestaurent,
+              let long = annotation.long,
+              let lat = annotation.lat else {
+                  return
+              }
+        
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        takeSnapshot(with: location)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func takeSnapshot(with location: CLLocationCoordinate2D) {
+        let mapSnapshotOptions = MKMapSnapshotter.Options()
+        var loc = location
+        let polyline = MKPolyline(coordinates: &loc, count: 1)
+        let region = MKCoordinateRegion(polyline.boundingMapRect)
+        mapSnapshotOptions.region = region
+        mapSnapshotOptions.scale = UIScreen.main.scale
+        mapSnapshotOptions.size = CGSize(width: 340, height: 208)
+        mapSnapshotOptions.showsBuildings = true
+        mapSnapshotOptions.pointOfInterestFilter = .includingAll
+        let snapShoter = MKMapSnapshotter(options: mapSnapshotOptions)
+        snapShoter.start(){snapshot, error in
+            guard let snapshot = snapshot else {
+                return
+            }
+            UIGraphicsBeginImageContextWithOptions(mapSnapshotOptions.size, true, 0)
+            snapshot.image.draw(at: .zero)
+            let identifier = "custompin"
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            pinView.image = UIImage(named: "custom-annotation")!
+            let pinImage = pinView.image
+            var point = snapshot.point(for: location)
+            let rect = self.imgMap.bounds
+            if rect.contains(point) {
+                let pinCenterOffset = pinView.centerOffset
+                point.x -= pinView.bounds.size.width/2
+                point.y -= pinView.bounds.size.height/2
+                point.x += pinCenterOffset.x
+                point.y += pinCenterOffset.y
+                pinImage?.draw(at: point)
+            }
+            if let image = UIGraphicsGetImageFromCurrentImageContext() {
+                UIGraphicsEndImageContext()
+                DispatchQueue.main.async {
+                    self.imgMap.image = image
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
