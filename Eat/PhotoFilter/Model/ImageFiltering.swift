129fde733dde1601b4f1e131fbe9a6d13b423386
luht <luht@developbranch.cn>
2022-01-13 00:52:03 +0800

    feat: code complete: choose picture on review screen.

---
+++ Eat/PhotoFilter/Model/ImageFiltering.swift
@@ -0,0 +1,31 @@
+//
+//  ImageFiltering.swift
+//  Eat
+//
+//  Created by lu on 2022/1/12.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+protocol ImageFiltering {
+    func apply(filter: String, originalImage: UIImage) -> UIImage
+}
+
+extension ImageFiltering {
+    func apply(filter: String, originalImage: UIImage) -> UIImage {
+        let initialCIImage = CIImage(image: originalImage, options: nil)
+        let originalOrientation = originalImage.imageOrientation
+        guard let ciFilter = CIFilter(name: filter) else {
+            print("filter not found")
+            return UIImage()
+        }
+        
+        ciFilter.setValue(initialCIImage, forKey: kCIInputImageKey)
+        let context = CIContext()
+        let filteredCIImage = (ciFilter.outputImage)!
+        let filteredCGImage = context.createCGImage(filteredCIImage, from: filteredCIImage.extent)
+        return UIImage(cgImage: filteredCGImage!, scale: 1.0, orientation: originalOrientation)
+    }
+}
