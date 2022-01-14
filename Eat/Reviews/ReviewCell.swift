2983aa030fafa62bfb20068264f1b321925cf460
luht <luht@developbranch.cn>
2022-01-14 09:05:35 +0800

    feat: complete display reviews on detail screen

---
+++ Eat/Reviews/ReviewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewCell.swift
+//  Eat
+//
+//  Created by lu on 2022/1/14.
+//
+
+import UIKit
+
+class ReviewCell: UICollectionViewCell {
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblReview: UILabel!
+    @IBOutlet weak var ratingsView: RatingsView!
+}
